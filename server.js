3946345c0de5963a179e2bc5b49b13bd5b58179f
David Wieczorek <blackratio@gmail.com>
2017-08-29 19:25:00 +0200

    Add mongodb package

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var helmet = require('helmet');
-var cors = require('cors');
-var home = require('./routes/home');
-var test = require('./routes/test');
-var config = require('./config');
+var helmet = require("helmet");
+var cors = require("cors");
+var home = require("./routes/home");
+var test = require("./routes/test");
+var config = require("./config");
+var mongoClient = require("mongodb").MongoClient;
 
 app.use(helmet(), cors());
 
-app.get('/', home.homeTest);
-app.get('/test', test.requestTest);
+// Paramètres de connexion
+var urlDB = "mongodb://localhost/dwdb";
 
-app.listen(config.port, function () {
-  console.log('Example app listening on port: ' + config.port);
+// Connexion au serveur avec la méthode connect
+mongoClient.connect(urlDB, function(err, db) {
+  if (err) return console.error("Connection failed", err);
+  console.log("Connection successful on ", urlDB);
+
+  // Fermeture de la connexion
+  db.close();
+});
+
+app.get("/", home.homeTest);
+app.get("/test", test.requestTest);
+
+app.listen(config.port, function() {
+  console.log("Example app listening on port: " + config.port);
 });
